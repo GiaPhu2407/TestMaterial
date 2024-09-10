793717cada4b24443a59bda155424cd557e20e69
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-10 22:37:55 +0700

    10/9/24

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -42,6 +42,36 @@ import { NextRequest, NextResponse } from "next/server";
 
 //   );
 // }
+// export async function GET(request: NextRequest) {
+//   const searchParams = request.nextUrl.searchParams;
+//   const page: number = searchParams.get("page")
+//     ? Number(searchParams.get("page"))
+//     : 1;
+//   const limit_size: number = searchParams.get("limit_size")
+//     ? Number(searchParams.get("limit_size"))
+//     : 10;
+
+//   const skip = (page - 1) * limit_size;
+
+//   const totalRecords = await prisma.post.count();
+//   const totalPage = Math.ceil(totalRecords / limit_size);
+
+//   const data =await prisma.post.findMany({
+//     skip,
+//     take:limit_size
+//   })
+//   return NextResponse.json({
+//     data,
+//     meta :{
+//       totalRecords,
+//       totalPage,
+//       page,
+//       limit_size,
+//       skip,
+//     }
+//   })
+// }
+
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const page: number = searchParams.get("page")
@@ -49,25 +79,23 @@ export async function GET(request: NextRequest) {
     : 1;
   const limit_size: number = searchParams.get("limit_size")
     ? Number(searchParams.get("limit_size"))
-    : 10;
-
+    : 1;
   const skip = (page - 1) * limit_size;
-
   const totalRecords = await prisma.post.count();
   const totalPage = Math.ceil(totalRecords / limit_size);
 
-  const data =await prisma.post.findMany({
+  const data = await prisma.post.findMany({
     skip,
-    take:limit_size
-  })
+    take: limit_size,
+  });
   return NextResponse.json({
     data,
-    meta :{
+    meta: {
       totalRecords,
       totalPage,
       page,
       limit_size,
       skip,
-    }
-  })
+    },
+  });
 }
